a16433f5ab077e1f5510bda55372d9754f0f0c2b
Rimuru-san <yann.casanova1607@gmail.com>
2022-05-11 13:20:06 -0300

    commit on 'develop'

---
+++ model/components/seguranca.js
@@ -0,0 +1,10 @@
+//npm install sha1 --- API
+function ocultarSenha(senha){
+    var sha1 = require('sha1');
+    console.log(senha);
+    var hash = sha1(senha);
+    console.log(hash);
+    return hash;
+}
+
+module.exports = {ocultarSenha}
